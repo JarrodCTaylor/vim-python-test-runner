7846adfcc3d5030b421a04ab253aa19ddc86436d
ryneeverett <ryneeverett@gmail.com>
2014-09-24 16:25:38 -0400

    Allow the NosetestMethod to run (free) test functions.

--- ftplugin/python/tests/run_nosetests_in_vim_tests.py
+++ ftplugin/python/tests/run_nosetests_in_vim_tests.py
@@ -38,6 +38,13 @@ class RunNosetestsInVimTests(unittest.TestCase):
         command_to_run = sut.get_command_to_run_current_method_with_nosetests(path_to_current_file, current_line, current_buffer)
         self.assertEqual(":!nosetests /tmp/project/tests/aTestFile.py:Example3.double_dummy1", command_to_run)
 
+    def test_create_command_to_run_current_method_with_nosetests_function(self):
+        path_to_current_file = "/tmp/project/tests/aTestFile.py"
+        current_line = 59
+        current_buffer = self.build_buffer_helper()
+        command_to_run = sut.get_command_to_run_current_method_with_nosetests(path_to_current_file, current_line, current_buffer)
+        self.assertEqual(":!nosetests /tmp/project/tests/aTestFile.py:example4", command_to_run)
+
     def test_create_command_to_run_current_method_with_nosetests_writes_command_to_cache_file_when_successfully_called(self):
         path_to_current_file = "/tmp/project/tests/aTestFile.py"
         current_line = 44
